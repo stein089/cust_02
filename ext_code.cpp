69c2f1a0928efc0afc086f03a1d5c9f8dd562465
Josef Steinbaeck <j.steinbaeck@arti-robots.com>
2021-05-05 16:33:00 +0200

    added extended code functionality

---
+++ ext_code.cpp
@@ -0,0 +1,19 @@
+extended function 0()
+{
+	// function 0
+}
+
+extended function 1()
+{
+	// function 1
+}
+
+extended function 2()
+{
+	// function 2
+}
+
+extended function 3()
+{
+	// function 3
+}
